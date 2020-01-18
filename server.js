e247dc831a4fa519f39df4492efe9e8e256c45f6
Brady Liechty <berrybloxinator@gmail.com>
2020-01-18 16:35:14 -0700

    Bug fix where empty spaces accepted as command arguments

--- server.js
+++ server.js
@@ -27,21 +27,25 @@ let server = net.createServer(client => {
             if (whisperCommand.length >= 3) {
                 const name = whisperCommand[1];
                 const message = whisperCommand.slice(2, whisperCommand.length).join(" ").trim();
-                let recipient;
-                for (let user of users) {
-                    if (user.name === name) {
-                        recipient = user;
-                    }
-                }
-                if(recipient === undefined) {
-                    client.write(`${name} is not online or name is not spelled correctly\ntry again\n`)
-                } else if (recipient.name === client.name) {
-                    client.write("You cannot use the /w command on yourself\n");
+                if (!name || !message) {
+                    client.write("Command incorrectly entered\nExample: /w Client5 Hello\n");
                 } else {
-                    writeToChatLog(`${client.name} said [${message}] to: ${recipient.name}\n`);
-                    recipient.write(`***\nWhisper from ${client.name}: ${message}\n***`);
-                    client.write(`Message sent to ${recipient.name}\n`);
-                    console.log(`${client.name} said [${message}] to: ${recipient.name}`);
+                    let recipient;
+                    for (let user of users) {
+                        if (user.name === name) {
+                            recipient = user;
+                        }
+                    }
+                    if(recipient === undefined) {
+                        client.write(`${name} is not online or name is not spelled correctly\ntry again\n`)
+                    } else if (recipient.name === client.name) {
+                        client.write("You cannot use the /w command on yourself\n");
+                    } else {
+                        writeToChatLog(`${client.name} said [${message}] to: ${recipient.name}\n`);
+                        recipient.write(`***\nWhisper from ${client.name}: ${message}\n***`);
+                        client.write(`Message sent to ${recipient.name}\n`);
+                        console.log(`${client.name} said [${message}] to: ${recipient.name}`);
+                    }
                 }
             } else if (whisperCommand.length < 3) {
                 client.write("Command incorrectly entered\nExample: /w Client5 Hello\n");
@@ -50,7 +54,9 @@ let server = net.createServer(client => {
             const usernameCommand = data.split(" ");
             if (usernameCommand.length === 2) {
                 const name = usernameCommand[1].trim();
-                if (name === client.name) {
+                if (!name) {
+                    client.write("Command incorrectly entered\nExample: /username betty\n");
+                } else if (name === client.name) {
                     client.write(`Your name is already ${client.name}`);
                 } else {
                     writeToChatLog(`${client.name} successfully changed their name to ${name}\n`);
@@ -67,26 +73,30 @@ let server = net.createServer(client => {
             if (kickCommand.length === 3) {
                 const name = kickCommand[1];
                 const password = kickCommand[2].trim();
-                if (password === adminPassword) {
-                    let recipient;
-                    for (let user of users) {
-                        if (user.name === name) {
-                            recipient = user;
-                        }
-                    }
-                    if (recipient === undefined) {
-                        client.write(`${name} is not online or name is not spelled correctly\ntry again\n`)
-                    } else if (recipient.name === client.name) {
-                        client.write("You cannot kick yourself\n");
-                    } else {
-                        writeToChatLog(`${client.name} kicked ${recipient.name}\n`);
-                        writeMessageToAllOtherUsers(`${client.name} kicked ${recipient.name}\n`, client);
-                        client.write(`You successfully kicked ${name}\n`);
-                        console.log(`${client.name} kicked ${recipient.name}`);
-                        recipient.end();
-                    }
+                if (!name || !password) {
+                    client.write("Command incorrectly entered\nExample: /kick Client3 [admin password]\n");
                 } else {
-                    client.write("That is not the admin password");
+                    if (password === adminPassword) {
+                        let recipient;
+                        for (let user of users) {
+                            if (user.name === name) {
+                                recipient = user;
+                            }
+                        }
+                        if (recipient === undefined) {
+                            client.write(`${name} is not online or name is not spelled correctly\ntry again\n`)
+                        } else if (recipient.name === client.name) {
+                            client.write("You cannot kick yourself\n");
+                        } else {
+                            writeToChatLog(`${client.name} kicked ${recipient.name}\n`);
+                            writeMessageToAllOtherUsers(`${client.name} kicked ${recipient.name}\n`, client);
+                            client.write(`You successfully kicked ${name}\n`);
+                            console.log(`${client.name} kicked ${recipient.name}`);
+                            recipient.end();
+                        }
+                    } else {
+                        client.write("That is not the admin password");
+                    }
                 }
             } else if (kickCommand.length < 3 || kickCommand.length > 3) {
                 client.write("Command incorrectly entered\nExample: /kick Client3 [admin password]\n");
@@ -95,10 +105,15 @@ let server = net.createServer(client => {
             const messageAllCommand = data.split(" ");
             if (messageAllCommand.length >= 2) {
                 const message = messageAllCommand.slice(1, messageAllCommand.length).join(" ").trim();
-                writeToChatLog(`${client.name} said [${message}] to all other users\n`);
-                client.write("Message sent to all other users\n");
-                writeMessageToAllOtherUsers(`Message from ${client.name}: ${message}`, client);
-                console.log(`${client.name} said [${message}] to all other users\n`);
+                if (!message) {
+                    client.write("Command incorrectly entered\nExample: /messageall Hello\n");
+                } else {
+                    console.log("message: ", message);
+                    writeToChatLog(`${client.name} said [${message}] to all other users\n`);
+                    client.write("Message sent to all other users\n");
+                    writeMessageToAllOtherUsers(`Message from ${client.name}: ${message}`, client);
+                    console.log(`${client.name} said [${message}] to all other users\n`);
+                }
             } else if (messageAllCommand.length < 2) {
                 client.write("Command incorrectly entered\nExample: /messageall Hello\n");
             }
